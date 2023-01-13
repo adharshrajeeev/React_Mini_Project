9672953827914d9e3d010b8632d5bd83608ebea5
Adharsh R <adharshrajeeev2000@gmail.com>
2023-01-13 22:51:29 +0530

    bug fixed

--- client/src/Components/AdminComponents/Header/AdminHeader.js
+++ client/src/Components/AdminComponents/Header/AdminHeader.js
@@ -27,7 +27,7 @@ function AdminHeader() {
   return (
     <nav class="navbar navbar-expand-lg adminHeadernav">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#" style={{color:"white"}} >WELCOME ADMIN</a>
+      <a class="navbar-brand" href="/adminHome" style={{color:"white"}} >WELCOME ADMIN</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
